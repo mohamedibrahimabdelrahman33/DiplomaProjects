6ec93867385343863fc5eb2d5968a38ea970d9c0
MOHAMED <MOHAMED@MOHAMED-PC>
2017-05-25 00:30:50 +0200

    UART Test: Sending ADC Conversion Result to PC by UART to USB Module and Receiving Data from PC and Give an indication by LEDS

---
+++ UART_RXTX_LEDS/LCD_Cfg.h
@@ -0,0 +1,22 @@
+/*
+ * LCD_Cfg.h
+ *
+ *  Created on: Mar 10, 2017
+ *      Author: Mohamed Tarek
+ */
+
+#ifndef LCD_CFG_H_
+#define LCD_CFG_H_
+
+#define	DATA_PORT	1
+#define	RS_PIN		30
+#define  E_PIN		31
+
+#define High   1
+#define Low    0
+#define Iinput 0
+#define Output 1
+#define LCD_RowLength 16
+
+
+#endif /* LCD_CFG_H_ */
